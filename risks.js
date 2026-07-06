eda05122db60fe69259b4a3735dce3a94b7df5d3
SB <46297131+scrumbaster@users.noreply.github.com>
2026-07-06 18:36:50 -0400

    Moved memory dump tree to risks.js and added a new risk to the list of risks.

---
+++ risks.js
@@ -0,0 +1,108 @@
+window.risks = [
+  {
+    "name": "🩺Health",
+    "children": [
+      { "name": "Weight" },
+      { "name": "Vacations" },
+      { "name": "Remove moles from balls" },
+      { "name": "Annual check up" },
+      { "name": "B12" },
+      { "name": "Health problems" },
+      { "name": "Eating" },
+      { "name": "Exercising" },
+      {
+        "name": "🦷Dental",
+        "children": [
+          { "name": "Dental cleaning" }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "💰Finance",
+    "children": [
+      { "name": "Property taxes" },
+      { "name": "Tax return" },
+      { "name": "Financial literacy" },
+      { "name": "Market monitoring" },
+      { "name": "Open or close accounts" },
+      { "name": "Credit score" },
+      { "name": "Insurance" },
+      { "name": "Investments" },
+      { "name": "Trading Stratagies" },
+      { "name": "Sell gold and silver" },
+      { "name": "FSA reimburcement" },
+      { "name": "Long termn care insurance" }
+    ]
+  },
+  {
+    "name": "🚙Cars Care",
+    "children": [
+      { "name": "Check liquids" },
+      { "name": "Clean the windshild and mirrors" },
+      { "name": "Bateries" },
+      { "name": "Wipers" },
+      { "name": "Filters" },
+      { "name": "Oil change" },
+      { "name": "Breaks" },
+      { "name": "Tires" }
+    ]
+  },
+  {
+    "name": "🏠Home Care",
+    "children": [
+      { "name": "Lown mover maintanance" },
+      { "name": "Snow blower maintanance" },
+      { "name": "Heating maintenance" },
+      { "name": "Gutters" },
+      { "name": "Home improvments" },
+      { "name": "Termite protection" },
+      { "name": "Ants Protection" }
+    ]
+  },
+  {
+    "name": "👔Work",
+    "children": [
+      { "name": "Updating LinkedIn Profile and Resume" },
+      { "name": "Build-up skill set for the next job" },
+      { "name": "Building relationships with people" },
+      { "name": "Cloud learning" },
+      { "name": "Python learning" }
+    ]
+  },
+  {
+    "name": "🦁Leo",
+    "children": [
+      { "name": "Leo's credit cards and other payments" },
+      { "name": "Leo's medical needs" }
+    ]
+  },
+  {
+    "name": "🤼Relationships",
+    "children": [
+      { "name": "Igor Gerasin" },
+      { "name": "Vlad" },
+      { "name": "Masha" },
+      { "name": "Sasha" },
+      { "name": "Mario" },
+      { "name": "Igor Raspin" },
+      { "name": "Vartan" },
+      { "name": "Kostya" }
+    ]
+  },
+  {
+    "name": "🏖️Retirement",
+    "children": [
+      { "name": "Learn cooking" },
+      { "name": "Reparing stuff to save money" },
+      { "name": "Audiobooks" },
+      { "name": "Learn fixing shoes" },
+      { "name": "Catching small game" },
+      { "name": "Biking" },
+      { "name": "Gardening" },
+      { "name": "Fishing" },
+      { "name": "Organize Photos" },
+      { "name": "Compensate executive function" }
+    ]
+  }
+];
